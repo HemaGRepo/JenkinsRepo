33f9d2505277afe4fae502f2d261761028f84de0
HemaGRepo <hema.g@capgemini.com>
2018-04-24 13:48:15 +0530

    Add files via upload
    c1

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java...");
+		for(int i=1;i<=20;i++)
+			System.out.println("Welcome to Java "+i);
+		
+		System.out.println("********Thank You**********");
+
+	}
+
+}
